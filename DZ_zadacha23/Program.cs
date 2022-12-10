7c09ac7c5b19246074b9740520856d914d511cdc
HermanYarenko <teyonel@icloud.com>
2022-12-10 20:09:57 +0300

    zadacha23

---
+++ DZ_zadacha23/Program.cs
@@ -0,0 +1,35 @@
+﻿
+
+
+
+
+while (true)
+{
+    try
+    {
+        Console.Write("Введите число: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+        if (number > 0)
+        {
+            int i = 1;
+            while (i <= number)
+            {
+                Console.Write(Math.Pow(i,3));
+                if (i != number)
+                    Console.Write(",");
+                i++;
+            }
+            Console.WriteLine();
+        }
+        else
+        {
+            Console.WriteLine("Число меньше 1");
+        }
+    }
+    catch
+    {
+
+        Console.WriteLine("вы ошиблись это не число ");
+
+    }
+}
